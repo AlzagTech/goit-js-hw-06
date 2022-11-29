aba1d8955b3c06029b36782f36f17c3240aa5520
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-11-29 21:59:30 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeIngredientsListItem = (ingredient) => { 
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+
+  return ingredientEl;
+};
+
+// document.querySelector('#ingredients').append(...ingredients.map(makeIngredientsListItem));
+
+const ingredientsList = ingredients.map(makeIngredientsListItem);
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredientsEl.append(...ingredientsList);
+
