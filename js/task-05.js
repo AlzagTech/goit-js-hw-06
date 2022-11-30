bc66d03eb1f53a1352f5fbf83c523b71ea2b9c41
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-11-30 20:12:02 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    event.currentTarget.value
+      ? (refs.span.textContent = event.currentTarget.value)
+      : (refs.span.textContent = 'Anonymous');
+    
+};
