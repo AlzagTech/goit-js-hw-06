23b75e42cee1687fb7fa812db01c569100c2ba11
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-01 20:59:36 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  spanEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+};
+
+refs.btnEl.addEventListener('click', onMakeRandomBgColor);
+
+function onMakeRandomBgColor(event) {
+  refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  refs.spanEl.textContent = `${getRandomHexColor()}`;
+}
