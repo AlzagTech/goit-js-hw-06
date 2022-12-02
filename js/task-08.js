39ceb37d45690496bf6e98eca0655f64f343da4b
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-02 13:46:57 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,7 +6,7 @@ function onFormSubmit(event) {
   event.preventDefault();
   const formElements = event.currentTarget.elements;
 
-  const email = formElements.email.value;
+  const email = formElements.email.value.trim();
   const password = formElements.password.value;
 
   const formData = {
